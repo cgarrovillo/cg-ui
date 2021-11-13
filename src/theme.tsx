c3751992ccf3a975b0280db2502267e87416f737
Christian Garrovillo <6688713+cgarrovillo@users.noreply.github.com>
2021-11-13 16:44:20 -0700

    Self host font css but CDN the file -- testing

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
+import './fonts/PlusJakartaSans.css'
 
 export const theme = createTheme({
   palette: {
     primary: {
       main: '#001e3c',
     },
+  },
+  typography: {
+    fontFamily: [
+      'Plus Jakarta Sans',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Roboto',
+      '"Helvetica Neue"',
+      '"Segoe UI"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
   },
 })
 
